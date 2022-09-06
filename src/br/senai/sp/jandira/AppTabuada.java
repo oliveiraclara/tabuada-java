b1bff229e455b3052ddf4eb7c5d8be46d12c8b4e
CLARA MARTINS OLIVEIRA <22282080@CFP127ALUNO.LOCAL>
2022-09-06 11:20:29 -0300

    tela

--- src/br/senai/sp/jandira/AppTabuada.java
+++ src/br/senai/sp/jandira/AppTabuada.java
@@ -1,5 +1,9 @@
 package br.senai.sp.jandira;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import br.senai.sp.jandira.gui.FrameTabuada;
 import br.senai.sp.jandira.model.Tabuada;
 
 public class AppTabuada {
@@ -8,7 +12,7 @@ public class AppTabuada {
 
 		Tabuada tabuada = new Tabuada();
 		tabuada.multiplicando = 139;
-		tabuada.minimoMultiplicador = 120;
+ 		tabuada.minimoMultiplicador = 120;
 		tabuada.maximoMultiplicador = 1000;
 
 		String[] resultado = tabuada.getTabuada();
@@ -19,6 +23,16 @@ public class AppTabuada {
 			System.out.println(resultado[contador]);
 			contador++;
 		}
+		
+		FrameTabuada tela = new FrameTabuada();
+		tela.titulo = "Tabuada";
+		tela.largura = 405;
+		tela.altura = 550;
+		tela.corDeFundo = new Color (250, 250, 250);
+		tela.fonteDosLabels = new Font ("BankGothic Md BT", Font.ITALIC, 20);
+		tela.corDoButton = new Color (255, 165, 0);
+		
+		tela.criarTela();
 
 	}
 
--- src/br/senai/sp/jandira/gui/FrameTabuada.java
+++ src/br/senai/sp/jandira/gui/FrameTabuada.java
@@ -1,5 +1,42 @@
 package br.senai.sp.jandira.gui;
 
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class FrameTabuada {
+	
+	public String titulo;
+	public int largura;
+	public int altura;
+	public Color corDeFundo;
+	public Font fonteDosLabels;
+	public Color corDoButton;
+	
+	public void criarTela() {
+		
+		JFrame tela = new JFrame();
+		tela.setTitle(titulo);
+		tela.setSize(largura, altura);
+		tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		tela.setLayout(null);
+		
+		Container painel = tela.getContentPane();
+		painel.setBackground(corDeFundo);
+		
+		JLabel labelTitulo = new JLabel();
+		labelTitulo.setText("Tabuada");
+		labelTitulo.setBounds(30, 30, 250, 90);
+		painel.add(labelTitulo);
+		labelTitulo.setFont(fonteDosLabels);
+		labelTitulo.setForeground(corDeFundo);
+		
+		tela.setVisible(true);
+		
+		
+	}
 
 }
